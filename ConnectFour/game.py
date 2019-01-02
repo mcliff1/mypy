b580067bd9c6187efd46bd59ee4b57e299472c06
Matt Cliff <matt.cliff@nordstrom.com>
2019-01-02 15:55:14 -0700

    work on game cleanup

--- ConnectFour/game.py
+++ ConnectFour/game.py
@@ -4,49 +4,50 @@
 Tic Tac Toe OO game
 """
 import logging
-import random
+#import random
 
 from board import Board
+from player import Player
 
 LOGGER = logging.getLogger()
 LOGGER.setLevel(logging.DEBUG)
 
-class Player():
-    """
-    represents a random player
-
-    a player shouldn't have a marker? or should they
-
-    made this a super class and sub with real person and various AI's
-    """
-
-    def __init__(self, marker):
-        """
-        it is expected to be either 'X' or 'O' for this game,
-        as long as the two players have separate markers
-
-        We have name so that we can use different implementations X or O
-        """
-        self.marker = marker
-        # since we use random, lets seed it now
-        random.seed()
-
-    def move(self, board):
-        """
-        return true if the move is made
-         otherwise return false if unable to move
-        """
-        #return board.place_marker((0, 0), self.marker)
-        open_moves = list(board.list_open_positions())
-        if open_moves:
-            move = random.choice(open_moves)
-            return board.place_marker(move, self.marker)
-        # else no open moves
-        return False
-
-
-    def __repr__(self):
-        return '{}'.format(self.marker)
+# class Player():
+#     """
+#     represents a random player
+#
+#     a player shouldn't have a marker? or should they
+#
+#     made this a super class and sub with real person and various AI's
+#     """
+#
+#     def __init__(self, marker):
+#         """
+#         it is expected to be either 'X' or 'O' for this game,
+#         as long as the two players have separate markers
+#
+#         We have name so that we can use different implementations X or O
+#         """
+#         self.marker = marker
+#         # since we use random, lets seed it now
+#         random.seed()
+#
+#     def move(self, board):
+#         """
+#         return true if the move is made
+#          otherwise return false if unable to move
+#         """
+#         #return board.place_marker((0, 0), self.marker)
+#         open_moves = list(board.list_open_positions())
+#         if open_moves:
+#             move = random.choice(open_moves)
+#             return board.place_marker(move, self.marker)
+#         # else no open moves
+#         return False
+#
+#
+#     def __repr__(self):
+#         return '{}'.format(self.marker)
 
 
 class Game():
@@ -218,24 +219,10 @@ def main():
     """
     test method to call when invoked from main
     """
-    # if len(sys.argv) == 1:
-    #     TicTacToe().mainloop()
-    # else:
-    #     needEval = ['-degree']
-    #     args = sys.argv[1:]
-    #     opts = {}
-    #     # parse the opts and args two at a time
-    #     for i in range(0, len(args), +2):
-    #         if args[i] in needEval:
-    #             opts[args[i][1:]] = eval(args[i+1])
-    #         else:
-    #             opts[args[i][1:]] = args[i+1]
-    #         trace(opts)
-    #         apply(TicTacToe, (), opts).mainloop()
     player1 = Player('X')
     player2 = Player('O')
     series = Series([player1, player2])
-    series.run(100000)
+    series.run(100)
 
     series2 = Series()
     series2.run(2, trace=True)
