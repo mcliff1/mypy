7c7bd24387aeb371fcbdc41b6be2058bee4b96df
Matt Cliff <matt@denvercliffs.com>
2019-02-11 07:45:13 -0700

    class method

--- mytest/program/test_employee.py
+++ mytest/program/test_employee.py
@@ -5,23 +5,34 @@ from employee import Employee
 
 class TestEmployee(unittest.TestCase):
 
+    @classmethod
+    def setUpClass(cls):
+        print('setupClass')
+
+    @classmethod
+    def tearDownClass(cls):
+        print('tearDownClass')
+
+    def setUp(self):
+        self.emp_1 = Employee('Matt', 'Cliff', 50000)
+        self.emp_2 = Employee('mike', 'Smith', 60000)
+
+    def tearDown(self):
+        pass
+
     def test_email(self):
-        emp_1 = Employee('Matt', 'Cliff', 50000)
-        emp_2 = Employee('mike', 'Smith', 60000)
 
-        self.assertEqual(emp_1.email, 'Matt.Cliff@email.com')
-        emp1.first = 'John'
+        self.assertEqual(self.emp_1.email, 'Matt.Cliff@email.com')
+        self.emp_1.first = 'John'
 
-        self.assertEqual(emp_1.email, 'John.Cliff@email.com')
+        self.assertEqual(self.emp_1.email, 'John.Cliff@email.com')
 
-    def test_email(self):
-        emp_1 = Employee('Matt', 'Cliff', 50000)
-        emp_2 = Employee('mike', 'Smith', 60000)
+    def test_raise(self):
 
-        self.assertEqual(emp_1.email, 'Matt.Cliff@email.com')
-        emp1.first = 'John'
+        self.assertEqual(self.emp_1.pay, 50000)
+        self.emp_1.apply_raise()
 
-        self.assertEqual(emp_1.email, 'John.Cliff@email.com')
+        self.assertEqual(self.emp_1.pay, 52500)
 
 
 if __name__ == '__main__':
